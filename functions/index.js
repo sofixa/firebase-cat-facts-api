7b96c749fe40b88c1ed4780fe3665fab4e5b1d96
Adrian Todorov <adrian.ttodorov@gmail.com>
2018-09-17 22:09:34 +0200

    cleanup and comments

--- functions/index.js
+++ functions/index.js
@@ -139,17 +139,12 @@ const facts = [
 	"Elvis Presley’s Chinese name is Mao Wong, or Cat King"
 ]
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.send("Hello from Firebase!");
-});
-
+// List all cat facts
 exports.all = functions.https.onRequest((request, response) => {
 	response.send(facts);
 });
 
+// Get a random fact
 exports.fact = functions.https.onRequest((request, response) => {
 	response.send(facts[Math.floor(Math.random() * facts.length)])
 });
